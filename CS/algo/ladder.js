b765045385d5dba082efca136759c7ffb3eb5f3a
svnh <sekunovsky@gmail.com>
2015-07-10 11:07:36 -0700

    Shuffle and ladder prompts

---
+++ CS/algo/ladder.js
@@ -0,0 +1,5 @@
+n = that number reprsentedt number of steps on a laddre
+function ladder(n)
+how many ways i can traverse the ladder if I go either one step at a time or two steps
+
+
---
+++ CS/algo/shuffle.js
@@ -0,0 +1,8 @@
+// You are given a playlist of songs,
+// create a system that lets you shuffle the songs
+// (as in, no repeats and a constant random order
+// every loop through the playlist), with options to
+// go back and also to repeat the current song?
+// Followup: How can you optimize for space?
+
+
