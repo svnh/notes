84303049a08984d20bf7f17e7ec889889cbbe597
svnh <sekunovsky@gmail.com>
2015-07-24 20:34:35 -0700

    more sumexcept

--- CS/algo/sumExcept.js
+++ CS/algo/sumExcept.js
@@ -15,4 +15,42 @@ var sumExcept = function(nums) {
   return output;
 };
 
-console.log(sumExcept([1,2,3,4]));
+var sumExcept = function(nums) {
+  var num = 1;
+
+  for(var i = 0; i < nums.length; i++) {
+    num = num * nums[i];
+  }
+
+  for(var i = 0; i < nums.length; i++) {
+    nums[i] = num / nums[i];
+  }
+
+  return nums;
+};
+
+var sumExcept = function(nums) {
+  var right = [];
+  var left = [];
+  var output = [];
+  var jsum = 1;
+  var isum = 1;
+  var r, j;
+
+  for(var i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+    jsum = jsum * nums[j];
+    isum = isum * nums[i];
+    right[i] = isum;
+    left[j] = jsum;
+  }
+
+  for(var i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+    r = right[i - 1] ? right[i - 1] : 1;
+    l = left[i + 1] ? left[i + 1] : 1;
+    output[i] = r * l;
+  }
+
+  return output;
+};
+
+console.log(sumExcept([1, 2, 3, 4, 5]));
